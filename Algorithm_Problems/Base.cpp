a5ba4647c64fa700c954921ad0790068af53c0b3
Cqk <1121180602@qq.com>
2017-03-11 13:39:06 +0800

    添加项目文件。

---
+++ Algorithm_Problems/Base.cpp
@@ -0,0 +1,67 @@
+/*
+//穷举搜索问题
+长度n的数列A和整数m，判断A中任意几个元素相加是否能得到m,A中每个元素只能用一次。
+利用一个递归函数判断：
+
+int solve(int i,int m){
+	if(m==0)return 1;
+	if(i>=n)return 0;
+	int result=solve(i+1,m)||solve(i+1,m-A[i]);
+	return result;
+	}
+	//	不适用于n较大问题，复杂度为O（2^n），或用动态规划优化
+*/
+
+/*
+//	快速排序/分治法
+int Partiton(int A[], int p, int r) {
+int pivot = A[r];
+int i = p - 1;
+for (int j = p; j < r ; ++j) {
+if (A[j] <= pivot) { i += 1; std::swap(A[i], A[j]); }
+}
+std::swap(A[i + 1], A[r]);
+return i + 1;
+}
+
+void Quick_Sort(int A[], int p, int r) {
+int q;
+if (p < r) {
+q = Partiton(A, p, r);
+Quick_Sort(A, p, q - 1);
+Quick_Sort(A, q+1,r);
+}
+}
+
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+
+int Partiton(int A[], int p, int r) {
+	int pivot = A[r];
+	int i = p - 1;
+	for (int j = p; j < r; ++j) {
+		if (A[j] <= pivot) { i += 1; std::swap(A[i], A[j]); }
+	}
+	std::swap(A[i + 1], A[r]);
+	return i + 1;
+}
+void Quick_Sort(int A[], int p, int r) {
+	int q;
+	if (p < r) {
+		q = Partiton(A, p, r);
+		Quick_Sort(A, p, q - 1);
+		Quick_Sort(A, q + 1, r);
+	}
+}
+int main() {
+	int A[] = { 9,6,3,2,5,8,7,4,1 };
+	Quick_Sort(A, 0, 9);
+	for (int i = 0; i < 9; ++i)cout << A[i];
+	return 0;
+}
