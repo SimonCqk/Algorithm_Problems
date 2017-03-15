e0064082eb0393c6cae5db4eb24d17a423b5e600
Cqk <1121180602@qq.com>
2017-03-15 12:40:33 +0800

    add 2_d_array version

--- Algorithm_Problems/Base.cpp
+++ Algorithm_Problems/Base.cpp
@@ -11,11 +11,34 @@ int solve(int i,int m){
 
 #include<iostream>
 #include<algorithm>
-#include<string>
-
+//#include<string>
+#define MAX_Coupon 100001
+#define MAX_Gift 500
 using namespace std;
 
-int main() {
+int Solve(int Gift_num, int Coupon_num, long long  Need[], int Value[], int Best[][MAX_Coupon]) {
+	int i, j;
+	for (j = 0; j < Coupon_num; ++j)Best[0][j] = 0;
+	for (i = 1; i <= Gift_num; ++i) {
+		for (j = 0; j <=Coupon_num; ++j) {
+			if (j < Need[i])Best[i][j] = Best[i - 1][j];
+			else Best[i][j] = max(Best[i-1][j], Best[i-1][j - Need[i]] + Value[i]);
+		}
+	}
+	return Best[Gift_num][Coupon_num];
 
+}
+
+
+long long Need[MAX_Coupon];
+int Value[MAX_Coupon], Best[MAX_Gift][MAX_Coupon];
+int main() {
+	int Gift_num, Coupon_num;
+	cin >> Gift_num >> Coupon_num;
+	for (int i = 1; i <= Gift_num; ++i) {
+		cin >> Need[i] >> Value[i];
+	}
+	int result = Solve(Gift_num, Coupon_num, Need, Value, Best);
+	cout << result << endl;
 	return 0;
 }
--- Algorithm_Problems/Floder_of_OJ/hihocoder_1038_01背包.cpp
+++ Algorithm_Problems/Floder_of_OJ/hihocoder_1038_01背包.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 #include<algorithm>
 //#include<string>
-#define MAX_SIZE 100001
+#define MAX_Coupon 100001
+#define MAX_Gift 501
 using namespace std;
 
+/*
+//二维数组版本
+//int Best[MAX_Gift][MAX_Coupon]
+int Solve(int Gift_num, int Coupon_num, long long  Need[], int Value[], int Best[][MAX_Coupon]) {
+	int i, j;
+	for (j = 0; j < Coupon_num; ++j)Best[0][j] = 0;
+	for (i = 1; i <= Gift_num; ++i) {
+		for (j = 0; j <=Coupon_num; ++j) {
+			if (j < Need[i])Best[i][j] = Best[i - 1][j];
+			else Best[i][j] = max(Best[i-1][j], Best[i-1][j - Need[i]] + Value[i]);
+		}
+	}
+	return Best[Gift_num][Coupon_num];
+
+}
+*/
 int Solve(int Gift_num, int Coupon_num, long long  Need[], int Value[], int Best[]) {
 	int i, j;
 	for (j = 0; j < Coupon_num; ++j)Best[j] = 0;
@@ -17,8 +34,8 @@ int Solve(int Gift_num, int Coupon_num, long long  Need[], int Value[], int Best
 }
 
 
-long long Need[MAX_SIZE];
-int Value[MAX_SIZE], Best[MAX_SIZE];
+long long Need[MAX_Gift];
+int Value[MAX_Gift], Best[MAX_Coupon];
 int main() {
 	int Gift_num, Coupon_num;
 	cin >> Gift_num >> Coupon_num;
