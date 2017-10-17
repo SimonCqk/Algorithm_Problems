fdff45eaaec03ba58f9380433e7670a5869d5fc3
Cqk <cqk0100@gmail.com>
2017-10-17 20:40:21 +0800

    add two solution.

---
+++ Algorithm_Problems/online_judge/leetcode_/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,34 @@
+/*
+Given a string, find the length of the longest substring without repeating
+characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3.
+Note that the answer must be a substring, "pwke" is a subsequence and not a
+substring.
+*/
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    if (!s.size()) {
+      return 0;
+    }
+    int size = s.size();
+    int ans = 0, start = -1;
+    std::vector<int> char_idxs(256, -1);
+    for (int i = 0; i < size; ++i) {
+      if (char_idxs[s[i]] > start)
+        start = char_idxs[s[i]];
+      if ((i - start) > ans)
+        ans = i - start;
+      char_idxs[s[i]] = i;
+    }
+    return ans;
+  }
+};
---
+++ Algorithm_Problems/online_judge/leetcode_/TwoSum.cpp
@@ -0,0 +1,26 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add
+up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    size_t size = nums.size();
+    for (size_t i = 0; i < size; ++i) {
+      for (size_t j = 0; j < size; ++j) {
+        if (i != j && (nums[i] + nums[j]) == target)
+          return {i, j};
+      }
+    }
+  }
+};
