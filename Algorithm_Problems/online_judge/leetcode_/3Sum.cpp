552261f271b99e549ba5928fbd474f4a97a1a87b
Cqk <cqk0100@gmail.com>
2017-11-13 22:56:15 +0800

    add 3sum solution.

---
+++ Algorithm_Problems/online_judge/leetcode_/3Sum.cpp
@@ -0,0 +1,46 @@
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+[-1, 0, 1],
+[-1, -1, 2]
+]
+*/
+
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int left = 0, right = nums.size() - 1;
+		set<vector<int>> ans_set;
+		for (; left<right; ++left) {
+			int dest = -nums[left];
+			for (int i = left + 1, j = right; i < j; ) {
+				int sum = nums[i] + nums[j];
+				if (sum>dest)
+					--j;
+				else if (sum<dest)
+					++i;
+				else {
+					int m = nums[i], n = nums[j];
+					vector<int> three = { m,n,-dest };
+					ans_set.insert(three);
+					while (i < j && nums[i] == m)
+						i++;
+					while (i < j && nums[j] == n)
+						j--;
+				}
+			}
+			while (left<right && nums[left] == nums[left + 1])
+				++left;
+		}
+		vector<vector<int>> ans(ans_set.begin(), ans_set.end());
+		return ans;
+	}
+};
--- Algorithm_Problems/tests.cpp
+++ Algorithm_Problems/tests.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
-#include<thread>
-#include<future>
-using namespace std::chrono;
+#include<vector>
+#include<set>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	set<vector<int>> vec;
+	vec.insert({ 1,2,3 });
+	vec.insert({ 2,3,4 });
+	vec.insert({ 1,2,3 });
+	for (auto& v : vec) {
+		for (auto& item : v) {
+			cout << item << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
 
