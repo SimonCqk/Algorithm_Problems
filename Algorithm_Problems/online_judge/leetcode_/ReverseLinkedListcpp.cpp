95f8fc4c78a05d8f8ae2dd4bdad5d9e4e69d74d3
Cqk <cqk0100@gmail.com>
2017-11-30 21:08:42 +0800

    add Reverse Linked List solution

---
+++ Algorithm_Problems/online_judge/leetcode_/ReverseLinkedListcpp.cpp
@@ -0,0 +1,37 @@
+/*
+Reverse a singly linked list.
+*/
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode* prev = nullptr;
+		ListNode* first = head;
+		while (first)
+		{
+			auto tmp = first->next;
+			first->next = prev;
+			prev = first;
+			first = tmp;
+		}
+		return prev;  // prev is new head
+	}
+};
+
+ListNode* reverseList(ListNode* head) {
+	if (!head || !head->next) 
+		return head;
+	ListNode* p = reverseList(head->next);
+	head->next->next = head;
+	head->next = nullptr;
+	return p;
+}
