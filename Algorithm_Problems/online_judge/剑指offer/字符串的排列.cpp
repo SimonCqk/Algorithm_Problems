b91f2db82bd37029ff0fc3dc77ac54d4e31a7da2
Cqk <cqk0100@gmail.com>
2018-01-23 22:35:54 +0800

    add “字符串的排列” 非递归 solution.

--- Algorithm_Problems/online_judge/剑指offer/字符串的排列.cpp
+++ Algorithm_Problems/online_judge/剑指offer/字符串的排列.cpp
@@ -28,8 +28,36 @@ void Permutation(string str) {
 	Permutation(str, 0);
 }
 
+// 非递归解法：(从最小字符串开始)
+// 从后往前找，直到找到第一对递增的数AB,然后找A后面的比A大的数中的最小数，交换位置
+// 交换后把原来A位置后面的字符串反转
+
+void Permutation_NonRecur(string str) {
+	str = std::move(str);
+	std::cout << str << std::endl;
+	//std::sort(str.begin(), str.end()); // 确保是最小字符串
+	int end = str.size() - 1;
+	int pos = end; // 需要交换的点
+	int next, min; // 交换点的下一点，交换点之后比它大的最小点
+	while (pos > 0) {
+		next = pos;
+		--pos;
+		if (str[pos] < str[next]) {
+			// 找到pos之后比它大的所有点中的最小点
+			// 从后往前找第一个比str[pos]大的数即是我们要找的数
+			min = end;
+			while (str[min] <= str[pos])
+				--min;
+			std::swap(str[pos], str[min]);
+			std::reverse(str.begin() + next, str.end());
+			std::cout << str << std::endl;
+			pos = end;  //回到最后
+		}
+	}
+}
+
 int main() {
 	string s = "abcd";
-	Permutation(s);
+	Permutation_NonRecur(s);
 	return 0;
 }
