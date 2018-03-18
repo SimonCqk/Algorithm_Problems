5851db7b0dde38326d57f490f005b915aa709763
Cqk <cqk0100@gmail.com>
2018-03-18 15:25:21 +0800

    add "Trapping Rain Water" dp solution.

--- Algorithm_Problems/online_judge/leetcode/TrappingRainWater.cpp
+++ Algorithm_Problems/online_judge/leetcode/TrappingRainWater.cpp
@@ -6,6 +6,7 @@ For example,
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 */
 #include<vector>
+#include<algorithm>
 
 using std::vector;
 
@@ -15,7 +16,7 @@ public:
 		if (height.empty())
 			return 0;
 		int trapped = 0;
-		auto beg = height.begin(), last = height.end()-1;
+		auto beg = height.begin(), last = height.end() - 1;
 		int max_left = 0, max_right = 0;
 		while (beg != last) {
 			if (*beg <= *last) {
@@ -33,6 +34,30 @@ public:
 				--last;
 			}
 		}
+		return trapped;
+	}
+};
+
+/*
+  Dynamic programming solution:
+  1.iterate from left to right, find left_max array.
+  2.iterate from right to left, find right_max array.
+  3.the overlap area is trapped area.
+*/
+class SolutionDp {
+public:
+	int trap(vector<int>& height) {
+		if (height.empty())
+			return 0;
+		int trapped = 0;
+		vector<int> left_max(height.size(), 0), right_max(height.size(), 0);
+		left_max[0] = height[0], right_max[height.size() - 1] = height[height.size() - 1];
+		for (int i = 1; i < height.size(); ++i)
+			left_max[i] = std::max(height[i], left_max[i - 1]);
+		for (int i = height.size() - 2; i >= 0; --i)
+			right_max[i] = std::max(height[i], right_max[i + 1]);
+		for (int i = 0; i < height.size(); ++i)
+			trapped += std::min(left_max[i], right_max[i]) - height[i];
 		return trapped;
 	}
 };
