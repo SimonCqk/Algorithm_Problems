babbbb5fae89a28d3bf76d926758d3110ca74711
Cqk <cqk0100@gmail.com>
2018-04-10 20:25:57 +0800

    add "Wildcard Matching dp" solution.

--- Algorithm_Problems/online_judge/leetcode/WildcardMatching.cpp
+++ Algorithm_Problems/online_judge/leetcode/WildcardMatching.cpp
@@ -16,6 +16,7 @@ isMatch("ab", "?*") ¡ú true
 isMatch("aab", "c*a*b") ¡ú false
 */
 #include<string>
+#include<vector>
 
 using namespace std;
 
@@ -25,8 +26,7 @@ public:
 		int idx_s = 0, idx_p = 0, idx_star = -1, idx_match = 0;
 		while (idx_s < s.size()) {
 			if (idx_p < p.size() && (s[idx_s] == p[idx_p] || p[idx_p] == '?')) {
-				idx_p++;
-				idx_s++;
+				idx_p++; idx_s++;
 			}
 			else if (idx_p < p.size() && p[idx_p] == '*') {  // can't match or '?', record pos of *
 				idx_star = idx_p;
@@ -48,4 +48,24 @@ public:
 		}
 		return idx_p == p.size();
 	}
+
+	bool isMatchDp(string s, string p) {
+		int s_size = s.size(), p_size = p.size();
+		vector<vector<bool>> dp(s_size + 1, vector<bool>(p_size + 1, false));
+		dp[0][0] = true;
+		for (int i = 1; i <= p_size; ++i)
+			if (p[i - 1] == '*')
+				dp[0][i] = true;
+			else
+				break;
+		for (int i = 1; i <= s_size; ++i)
+			for (int j = 1; j <= p_size; ++j) {
+				if (p[j - 1] != '*')
+					dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '?');
+				else
+					dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+			}
+		return dp[s_size][p_size];
+	}
 };
+
