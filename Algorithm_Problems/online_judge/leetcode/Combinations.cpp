2fc740cf56db79ca9581b5e819a5807b6d9dcfd5
Cqk <cqk0100@gmail.com>
2018-04-14 10:19:47 +0800

    add "Combinations" iterative solution.

--- Algorithm_Problems/online_judge/leetcode/Combinations.cpp
+++ Algorithm_Problems/online_judge/leetcode/Combinations.cpp
@@ -40,6 +40,22 @@ public:
 			}
 		}
 	}
+	vector<vector<int>> combineIterative(int n, int k) {
+		vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1)
+				result.push_back(p);
+			else {
+				++i;
+				p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
 };
 
 int main() {
