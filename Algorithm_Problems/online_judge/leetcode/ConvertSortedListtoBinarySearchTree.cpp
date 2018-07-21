81e18e4978005273f6decd20ed05a280532d95e6
bytedance <bytedance@bytedancedeMBP.lan>
2018-07-21 21:21:20 +0800

    add "Convert Sorted List to Binary Search Tree" solution.

---
+++ Algorithm_Problems/online_judge/leetcode/ConvertSortedListtoBinarySearchTree.cpp
@@ -0,0 +1,61 @@
+/*
+Given a singly linked list where elements are sorted in ascending order, convert
+it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in
+which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following
+height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+#include<vector>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+   public:
+    TreeNode *sortedListToBST(ListNode *head) {
+        if (!head) return nullptr;
+        vector<int> nums;
+        while (head) {
+            nums.push_back(head->val);
+            head = head->next;
+         }
+         return toTree(nums, 0, nums.size() - 1);
+    }
+
+   private:
+    TreeNode* toTree(const vector<int>& nums, int start, int end) {
+        if (start > end) return nullptr;
+        int mid = (start + end) / 2;
+        auto node = new TreeNode(nums[mid]);
+        node->left = toTree(nums, start, mid - 1);
+        node->right = toTree(nums, mid + 1, end);
+        return node;
+    }
+};
